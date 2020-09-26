c54810b8d2105fb573693546f6ebb70b87198179
dario <dariob450@gmail.com>
2020-09-26 13:34:15 +0200

    1.4
    Version update
    Added useful links to the VersionCommand

--- src/com/hetag/blockdisplays/commands/VersionCommand.java
+++ src/com/hetag/blockdisplays/commands/VersionCommand.java
@@ -23,6 +23,11 @@ public class VersionCommand extends BDCommand {
 		sendMessage(sender, "&3Author: &b" + BlockDisplays.getInstance().getDescription().getAuthors().toString().replace("[", "").toString().replace("]", ""), false);
 		sendMessage(sender, "&3Compatible Minecraft Version(s): &b 1.13 - 1.14 - 1.15 - 1.16", false);
 		sendMessage(sender, "&3Using Protocol: &b" + BlockDisplays.getProtocol().toString(), false);
+		sendMessage(sender, "", false);
+		sendMessage(sender, "&8« &bUseful links &8»", false);
+		sendMessage(sender, "&aSpigot page &8» &bhttps://www.spigotmc.org/resources/blockdisplays-decorate-your-server-1-13-x-1-14-x-1-15-x-1-16-x.76007/", false);
+		sendMessage(sender, "&aGithub &8» &bhttps://github.com/Hetag1216/BlockDisplays", false);
+		sendMessage(sender, "&aCheck out my other resources at &8» &bhttps://www.spigotmc.org/members/_hetag1216_.243334/", false);
+		sendMessage(sender, "&aDiscord &8» &bhttps://discord.gg/yqs9UJs", false);
 	}
-
 }
