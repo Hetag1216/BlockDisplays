59ea6a9ba8da759e9e8df7f964d277d11f0a670d
Hetag1216 <dariob450@gmail.com>
2023-09-18 12:57:10 +0200

    Added to/here options to teleport command

--- src/com/hedario/blockdisplays/commands/TeleportCommand.java
+++ src/com/hedario/blockdisplays/commands/TeleportCommand.java
@@ -12,50 +12,51 @@ import com.hedario.blockdisplays.configuration.Manager;
 public class TeleportCommand extends BDCommand {
 
 	public TeleportCommand() {
-		super("teleport", "/bd teleport <name>", formatColors(Manager.getConfig().getString("Commands.Teleport.Description")), new String[] { "teleport", "tp" });
+		super("teleport", "/bd teleport <name> <to|here>", formatColors(Manager.getConfig().getString("Commands.Teleport.Description")), new String[] { "teleport", "tp" });
 	}
 
 	@Override
 	public void execute(CommandSender sender, List<String> args) {
-		if (!hasPermission(sender) || !correctLength(sender, 0, 0, 1)) {
+		if (!hasPermission(sender) || !correctLength(sender, args.size(), 2, 2) || !isPlayer(sender)) {
 			return;
 		}
-		if (args.size() == 1) {
-			Player p = (Player) sender;
-			String block = args.get(0);
-			if (FloatingBlock.exists(block)) {
-				if (FloatingBlock.isAlive(block)) {
-					FloatingBlock.updateLocation(block, p.getLocation());
-					sendMessage(sender, onTeleport().replace("%name%", args.get(0)), true);
-					return;
-				} else {
-					sendMessage(sender, onInvalid().replace("%name%", args.get(0)), true);
-					return;
-				}
-			} else {
-				sendMessage(sender, notFound().replace("%name%", args.get(0)), true);
-				return;
-			}
+		final String name = args.get(0);
+		final Player player = (Player) sender;
+		if (!FloatingBlock.validateFloatingBlock(sender, name)) {
+			return;
+		}
+		if (args.get(1).equalsIgnoreCase("to")) {
+			player.teleport(FloatingBlock.getLocation(name));
+			this.sendMessage(sender, teleportTo().replace("%name%", name), true);
+		} else if (args.get(1).equalsIgnoreCase("here")) {
+			FloatingBlock.updateLocation(name, player.getLocation());
+			sendMessage(sender, teleportHere().replace("%name%", name), true);
+			return;
 		}
 	}
 
-	public String onTeleport() {
-		return Manager.getConfig().getString("Commands.Teleport.OnTeleport");
+	public String teleportHere() {
+		return Manager.getConfig().getString("Commands.Teleport.TeleportHere");
 	}
 	
-	private String notFound() {
-		return Manager.getConfig().getString("Commands.Teleport.NotFound");
-	}
-	
-	private String onInvalid() {
-		return Manager.getConfig().getString("Commands.Teleport.OnInvalid");
+	public String teleportTo() {
+		return Manager.getConfig().getString("Commands.Teleport.TeleportTo");
 	}
 	
 	@Override
 	protected List<String> getTabCompletion(final CommandSender sender, final List<String> args) {
-		if (!sender.hasPermission("blockdisplays.command.teleport") || args.size() > 1) {
+		List<String> list = new ArrayList<String>();
+		if (!sender.hasPermission("blockdisplays.command.teleport") || args.size() > 2) {
 			return new ArrayList<String>();
 		}
-		return FloatingBlock.getFloatingBlocks();
+		if (args.size() == 0) {
+			return FloatingBlock.getFloatingBlocks();
+		} else if (args.size() == 1) {
+			list.add("here");
+			list.add("to");
+		} else {
+			return new ArrayList<String>();
+		}
+		return list;
 	}
 }
